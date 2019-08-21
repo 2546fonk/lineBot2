1e23eaf52a11cb8a250a160eb074640a16d01a4e
2546fonk <51729234+2546fonk@users.noreply.github.com>
2019-08-21 09:17:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def callback():
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
     if (userText == 'สวัสดี') :
-       sendText(user'ดีจ้าาา')
+       sendText(user,'ดีจ้าาา')
     elif (userText == 'ทำไรอยู่') :
        sendText(user,'นั่ง')
     else :
